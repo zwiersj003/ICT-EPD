5c45b57091514e64dfdb5139a12f0633d9bf0c49
JasperZwiers <jasper.zwiers16@gmail.com>
2018-11-06 10:25:59 +0100

    Jasper: Afspraak Modal WIP

--- coreui-free-react-admin-template-master/src/views/Epd/Afspraken/Afspraken.js
+++ coreui-free-react-admin-template-master/src/views/Epd/Afspraken/Afspraken.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
-import { Button, Card, CardBody, CardHeader, Col, Row } from 'reactstrap';
+import React, { Component } from 'react'
+import { Button, Card, CardBody, CardHeader, Col, Row, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import AfspraakModal from '../../../components/Modal/AfspraakModal'
 
 class Afspraken extends Component {
     render() {
         return (
             <div className="animated fadeIn">
-                <Row>
-                    <Col sm="3">
-                        <Card>
-                            <CardHeader>
-                                <i className="fa fa-filter"></i> Filters
-                            </CardHeader>
-                            <CardBody>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col sm="9">
-                        <Card>
-                            <CardHeader>
-                                <i className="fa fa-calendar-o"></i> Afspraken
-                    </CardHeader>
-                            <CardBody>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
+                <div className="afpraken">
+                    <Row>
+                        <Col sm="3">
+                            <Card>
+                                <CardHeader>
+                                    <i className="fa fa-filter"></i> Filters
+                                </CardHeader>
+                                <CardBody>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col sm="9">
+                            <Card>
+                                <CardHeader>
+                                    <i className="fa fa-calendar-o"></i> Afspraken
+                                </CardHeader>
+                                <CardBody>
+                                    <AfspraakModal />
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         )
     }
