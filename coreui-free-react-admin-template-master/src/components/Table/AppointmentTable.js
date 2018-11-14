e32c8ba95e9d513a00c576b613281df6e502607d
JasperZwiers <jasper.zwiers16@gmail.com>
2018-11-14 09:36:42 +0100

    Jasper: TR click opens modal, not dynamic

--- coreui-free-react-admin-template-master/src/components/Table/AppointmentTable.js
+++ coreui-free-react-admin-template-master/src/components/Table/AppointmentTable.js
@@ -1,10 +1,99 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Button, Card, CardBody, CardHeader, Col, Row, Table, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
 
 class AfspraakTabel extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            modal: false,
+            large: false,
+            appointments: [
+                {
+                    appointmentID: 3,
+                    appointment: 'Spoedeisende Hulp',
+                    date: '09-12-2017',
+                    location: 'Amstelland Ziekenhuis'
+                },
+                {
+                    appointmentID: 2,
+                    appointment: 'Huisarts',
+                    date: '09-12-2017',
+                    location: 'Amstelland Ziekenhuis'
+                },
+                {
+                    appointmentID: 1,
+                    appointment: 'Oogheelkunde',
+                    date: '18-02-2017',
+                    location: 'VUMC'
+                },
+            ]
+        };
+        this.toggleLarge = this.toggleLarge.bind(this);
+    }
+
+    toggleLarge() {
+        this.setState({
+            large: !this.state.large,
+        });
+    }
+
+    renderAppointment = ({ appointmentID, appointment, date, location }) => <tr onClick={this.toggleLarge}><td>{appointmentID}</td><td>{appointment}</td><td>{date}</td><td>{location}</td></tr>
+
     render() {
+        const { appointments } = this.state
         return (
-            <div>
-                Hello World
+            <div className="appointment-table">
+                <Table responsive striped hover bordered id="table-modules">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Afspraak</th>
+                            <th>Datum</th>
+                            <th>Locatie</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {appointments.map(this.renderAppointment)}
+                        <Modal isOpen={this.state.large} toggle={this.toggleLarge} className={'modal-lg ' + this.props.className}>
+                            <ModalHeader toggle={this.toggleLarge}>Titel</ModalHeader>
+                            <ModalBody>
+                                <div id="appointment-info">
+                                    <Row>
+                                        <Col md="6">
+                                            <p><b>Locatie:</b> AMC</p>
+                                            <p><b>Datum:</b> 23-11-2018</p>
+                                            <p><b>Tijd:</b> 10:30</p>
+                                        </Col>
+                                        <Col md="6">
+                                            <p><b>Afdeling:</b> Polikliniek</p>
+                                            <p><b>Dokter:</b> R. Michels</p>
+                                        </Col>
+                                    </Row>
+                                </div>
+                                <div className="extra-info">
+                                    <Row>
+                                        <Col md="6">
+                                            <div id="description">
+                                                <h4>Omschrijving</h4>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit tristique ex, non sollicitudin mi molestie quis. Sed condimentum imperdiet ex, in viverra nisi molestie nec. Quisque et congue turpis, eget rhoncus ex. Curabitur elementum enim ac mollis maximus. Pellentesque a euismod lorem. Ut vel dolor libero. Integer dictum venenatis lorem. Proin bibendum varius massa, sed volutpat purus varius quis. Quisque quis mi at libero facilisis faucibus non vitae urna. Fusce mattis pulvinar nunc id dapibus. Cras et condimentum dui. Phasellus nec nisi in tortor auctor porta. Aliquam id nulla nibh.
+                                </div>
+                                        </Col>
+                                        <Col md="6">
+                                            <div id="treatment">
+                                                <h4>Behandeling</h4>
+                                                Lorem ipsum
+                                </div>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="primary" onClick={this.toggleLarge}>Sluiten</Button>
+                            </ModalFooter>
+                        </Modal>
+                    </tbody>
+                </Table>
             </div>
         );
     }
